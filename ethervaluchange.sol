398e04c526adff397cbf7017d08efc069dbf94f8
kavinmayil <168561418+kavinmayil@users.noreply.github.com>
2024-05-12 00:33:23 +0530

    Create ethervaluchange.sol

---
+++ ethervaluchange.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract practice{
+    // function
+    /*
+    function functionName(argument) visibility state mutability return(data type){
+        } visibility-1.public,2.private,3.internal,4.external// state la irrukura data vaa read panna-view,ethum pannalana-pure,write panna-ethum kuduka kudathu
+    
+    */  //payable keyword is used to call FundMe function to repair ether
+    
+    //function FundMe()public payable { } 
+    function FundMe()public { }
+
+    }
+    
+// simple unit converter 
+ // wei-1000000000000000000
+ // gwei-1000000000
+ // ether-1
+    
+ 
